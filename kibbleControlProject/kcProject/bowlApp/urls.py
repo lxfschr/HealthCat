79006abd52cbf6afa2204ab3815b0f5a9c816add
Vasif Syed <vasif@live.co.uk>
2014-11-07 18:44:55 -0500

    more functions for kibble control

--- kibbleControlProject/kcProject/bowlApp/urls.py
+++ kibbleControlProject/kcProject/bowlApp/urls.py
@@ -6,8 +6,16 @@ urlpatterns = patterns('',
     # url(r'^$', 'kcProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^openornot/(?P<catID>\d+)/(?P<timeNow>\d\d\d\d)/(?P<YYYYMMDD>\d\d\d\d\d\d\d\d)$','bowlApp.views.openornot'),
-    url(r'^rfidtopetid/(?P<rfid>\d+)$','bowlApp.views.rfidtopetid'),
-    url(r'^', include('bowlApp.urls')),
+    url(r'^openornot/(?P<rfid>\d+)/(?P<timenow>\d\d\d\d)/(?P<YYYYMMDD>\d\d\d\d\d\d\d\d)$','bowlApp.views.openornot'),
+    # url(r'^rfidtopetid/(?P<rfid>\d+)$','bowlApp.views.rfidtopetid'), ## depricated
+    url(r'^createpet/(?P<rfid>\d+)','bowlApp.views.createPet'),
+    url(r'^createschedule/(?P<nameOfSch>[a-zA-Z0-9]+)','bowlApp.views.createSchedule'),
+    url(r'^updatescheduleadd/(?P<schID>\d+)/(?P<timein>\d\d\d\d)/(?P<timeout>\d\d\d\d)/(?P<amount>\d+)',
+    	'bowlApp.views.updateScheduleAdd'),
+    url(r'^updatescheduleremove/(?P<tawID>[a-zA-Z0-9]+)',
+    	'bowlApp.views.updateScheduleRemove'),
+    url(r'^createpet/(?P<rfid>\d+)','bowlApp.views.createPet'),
+    url(r'^connect','bowlApp.views.connect'),
+
 
 )
--- kibbleControlProject/kcProject/bowlApp/views.py
+++ kibbleControlProject/kcProject/bowlApp/views.py
@@ -10,8 +10,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate
 
-from grumblr.models import *
-from grumblr.forms import *
+from bowlApp.models import *
+# from bowlApp.forms import *
 
 from django.db import transaction# Create your views here.
 
@@ -27,16 +27,20 @@ from django.contrib.auth.tokens import default_token_generator
 
 
 # to make http requests. 
-import urllib2,urllib,httplib
+import urllib2,urllib,httplib,json
+
+#time related library
+import time, datetime
 
 ###########################################################################
 
+BOWLID = 1
 
-def openornot(rfidID,timenow,YYYYMMDD):
+def openornot(request,rfid,timenow,YYYYMMDD):
 
 	responseDict= {}
 
-	if openornotLogic(catID,timenow,YYYYMMDD):
+	if openornotLogic(rfid,int(timenow),YYYYMMDD):
 		responseDict['result'] = 'true'
 
 		return HttpResponse(json.dumps(responseDict),
@@ -47,28 +51,125 @@ def openornot(rfidID,timenow,YYYYMMDD):
 		return HttpResponse(json.dumps(responseDict),
 			content_type='application/json')		
 
-
 # this holds the logic
-def openornotLogic(catID,timenow,YYYYMMDD):
+# it should return true if the bowl can open else not.
+def openornotLogic(rfid,timenow,YYYYMMDD):
+
 	# having a simple logic now. 
-	# if its day time open else close for any cat. 
+	# if its day time open else close for any cat.
 	return timenow >800 and timenow< 2200
 
-
 # making logs of when the cat ate.
-def catJustAte(amount,timeStart,timeEnd,YYYYMMDD):
+def catJustAte(request, amount,timeStart,timeEnd,YYYYMMDD):
+
 	urltopost="http://healthcat.herokuapp.com/record"
 	data= urllib2.urlencode({'amount':amount, 'tStart':timeStart, 
 							'tEnd':timeEnd, 'YYYYMMDD':YYYYMMDD})
+	conn= httplib.HTTPConnection(urltopost)
 	headers = {"content_type": "application/x-www-form-urlencoded",
 				"Accept":"text/plain"}
 
-	h.request('POST', urltopost ,data,headers)
-
-
+	conn.request('POST','/record',data,headers)
+	response= conn.getresponse()
 	pass
 
 
+def createPet(request,rfid):
+
+	#validation for rfid
+
+	#new pet creation
+	newPet=Pet(rfid=rfid)
+	newPet.save()
+	responseDict={}
+	responseDict['result']='Success'
+	responseDict['petID'] = newPet.id
+
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')	
+
+def createSchedule(request,nameOfSch):
+
+	newFoodSch= FoodSchedule()
+	newFoodSch.name=nameOfSch
+	newFoodSch.save()
+
+	#respond with schID
+	responseDict={}
+	responseDict['result'] = 'success'
+	responseDict['schID'] = newFoodSch.id
+
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')	
+
+def updateScheduleAdd(request,schID,timein,timeout,amount):
+	# validating the timein timeout and amount .
+
+	# convert time in to timestamp
+	timein,timeout=int(timein),int(timeout)
+	dtTimeIn = datetime.time(timein/100,timein%100)
+	dtTimeOut = datetime.time(timeout/100,timeout%100)
+
+	# make a new schedule and add it to a pet.
+	responseDict={}
+	try:
+		fs = FoodSchedule.objects.get(id=schID)
+	except:
+		responseDict['result'] = 'Fail'
+		return HttpResponse(json.dumps(responseDict),
+			content_type='application/json')
+
+
+	newTimeAmountWindow= TimeAmountWindow(timeOpen=dtTimeIn,
+			timeClose=dtTimeOut,amount=amount,relatedSchedule=fs)
+	newTimeAmountWindow.save()
+	responseDict['result'] = 'success'
+	responseDict['tawID'] = newTimeAmountWindow.id
+
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')
+
+
+def updateScheduleRemove(request,tawID):
+	# validating the timein timeout and amount .
+
+	# make a new schedule and add it to a pet.
+	responseDict={}
+	try:
+		fs = TimeAmountWindow.objects.get(id=schID)
+	except:
+		responseDict['result'] = 'Fail'
+		return HttpResponse(json.dumps(responseDict),
+			content_type='application/json')
+
+
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')
+
+def assignPetASchedule(request,rfid,schID):
+
+	responseDict={}
+	try:
+		fs = FoodSchedule.objects.get(id=schID)
+		p = Pet.objects.get(rfid=rfid)
+	except:
+		responseDict['result'] = 'Fail'
+		return HttpResponse(json.dumps(responseDict),
+			content_type='application/json')
+	p.foodSchedule_set(fs)
+	responseDict['result'] = 'Success'
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')
+
+
+def connect(request):
+	
+	responseDict={}
+	responseDict['result'] = 'Success'
+	responseDict['bowlID'] = BOWLID
+
+	return HttpResponse(json.dumps(responseDict),
+		content_type='application/json')
 
 
 
