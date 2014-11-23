a0f9bebd594c46706c86549c0173f7a858f598e1
Vasif Syed <vasif@live.co.uk>
2014-11-22 19:19:09 -0500

    robust webapi with ocks

--- kibbleControlProject/test.py
+++ kibbleControlProject/test.py
@@ -1,7 +1,32 @@
 import webapi
-
+# import time
+# from bowl_settings import *
+# import json
 # print webapi.validateBowl()
+# indexToRfid={'b':'4'}
+
+# print webapi.newRfidDetected(3)
+
+# filename='test.txt'
+
+# a= open(filename,'w+')
+# json.dump(indexToRfid,a)
+
+# print 'sleeping..'
+# for i in xrange(0,10):
+# 	print (10-i) ,
+# 	time.sleep(1)
+
+# print 'awake....'
+# a.close()
 
 
-# print webapi.newRfidDetected(0)
-print webapi.openOrNot(0)
+
+
+# print webapi.openOrNot(0)
+
+
+# import webapi2
+# print webapi2.acquireLock()
+
+# print hello
