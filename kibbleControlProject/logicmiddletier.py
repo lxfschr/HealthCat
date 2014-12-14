f90eac46c80db24c981ee59a1f51e57d24b5bb1e
Vasif Syed <vasif@live.co.uk>
2014-12-14 04:08:03 -0500

    controller.py made for rfid and button press

--- kibbleControlProject/logicmiddletier.py
+++ kibbleControlProject/logicmiddletier.py
@@ -5,80 +5,82 @@ import time
 
 def init():
 
-	#keep reading rfid
-	rfidList=[]
-	eatTimeStart=None
-	eatWeightStart=None
+    #keep reading rfid
+    rfidList=[]
+    eatTimeStart=None
+    eatWeightStart=None
 
-	while (True):
+    while (True):
 
-		rfidList = controlFunctions.readRFID()
-		numOfCats = len(rfidList)
+        rfidList = controlFunctions.readRFID()
+        numOfCats = len(rfidList)
 
-		if numOfCats==prevNumCats:
-			numOfCatsNotChanged +=1
+        if numOfCats==prevNumCats:
+            numOfCatsNotChanged +=1
 
-		else:
-			numOfCatsNotChanged +=1
-			prevNumCats = numOfCats
+        else:
+            numOfCatsNotChanged +=1
+            prevNumCats = numOfCats
 
-		if numOfCatsNotChanged>5 and numOfCats == 0:
-			makeStateClosed()
+        if numOfCatsNotChanged>5 and numOfCats == 0:
+            makeStateClosed()
 
-		elif  numOfCatsNotChanged> 5 and numOfCats==1:
+        elif  numOfCatsNotChanged> 5 and numOfCats==1:
 
-			# get the rfid and see if it is valid
-			rfid = rfidList[0]
+            # get the rfid and see if it is valid
+            rfid = rfidList[0]
 
-			t= webapi.openOrNot(rfid)
-			if (t==-1):
-				# case where the new rfid needs to be created
-				result =newRfidDetected(rfid)
-				print result + ' is the result 1 means new rfid created 0 means not'
-			
-			elif t==0:
-				# bowl should not be opened its 0 grams or seconds
-				makeStateClosed()
-				eatTimeStart=None
-				eatWeightStart=None
-			
-			elif t>0:
-				# case where the  bowl needs to be opened for so many seconds
+            t= webapi.openOrNot(rfid)
+            if (t==-1):
+                # case where the new rfid needs to be created
+                result =newRfidDetected(rfid)
+                print result + ' is the result 1 means new rfid created 0 means not'
+            
+            elif t==0:
+                # bowl should not be opened its 0 grams or seconds
+                makeStateClosed()
+                eatTimeStart=None
+                eatWeightStart=None
+            
+            elif t>0:
+                # case where the  bowl needs to be opened for so many seconds
 
-				if not eatTimeStart:
-					eatTimeStart = datetime.datetime.now()
-					eatWeightStart = controller.getWeight()
-					makeStateOpen()
+                if not eatTimeStart:
+                    eatTimeStart = datetime.datetime.now()
+                    eatWeightStart = controller.getWeight()
+                    makeStateOpen()
 
-				else:
-					now=datetime.datetime.now()
-					weightNow = controller.getWeight()
+                else:
+                    now=datetime.datetime.now()
+                    weightNow = controller.getWeight()
 
-					if now - eatTimeStart > datetime.datetime(seconds=t)
-						and weightNow - eatWeightStart > 10:
+                    if now - eatTimeStart > datetime.datetime(seconds=t)
+                        and weightNow - eatWeightStart > 10:
 
-						makeStateClosed()
-						eatTimeStart=None
-						#make logging call here
+                        makeStateClosed()
+                        eatTimeStart=None
+                        #make logging call here
 
-		else if numOfCatsNotChanged>5 and numOfCats>1:
+        else if numOfCatsNotChanged>5 and numOfCats>1:
 
-			makeStateClosed()
-			eatTimeStart=None
-			eatWeightStart = None
-			# make a bullying call here.
+            makeStateClosed()
+            eatTimeStart=None
+            eatWeightStart = None
+            # make a bullying call here.
 
-		time.sleep(1)
+        time.sleep(1)
+
+
+
 
 def makeStateOpen():
-	if not controlFunctions.isOpen():
-		controlFunctions.open()
+    if not controlFunctions.isOpen():
+        controlFunctions.open()
 
 def makeStateClosed():
-	if controlFunctions.isOpen():
-		controlFunctions.close()
+    if controlFunctions.isOpen():
+        controlFunctions.close()
 
 def refillBowl():
-
-	makeStateClosed()
-	controlFunctions.refill()
+    makeStateClosed()
+    controlFunctions.refill()
